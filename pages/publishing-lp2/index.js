c1ae38ccbf44635d1eca25d4f5d26eddd4d722f6
PineBook2024 <pinebookpublishing@gmail.com>
2024-06-26 15:53:33 -0700

    cta changes lp

--- pages/publishing-lp2/index.js
+++ pages/publishing-lp2/index.js
@@ -439,47 +439,47 @@ export default function PublishingLpNew() {
                             </button>
                         ))} */}
                         <Swiper
-                className=""
-                spaceBetween={15}
-                slidesPerView={6}
-                loop={true}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
-                }}
-                onBeforeInit={(swiper) => {
-                    swiperRef.current = swiper;
-                }}
-                modules={[Navigation, Autoplay, Pagination]}
-                breakpoints={{
-                    "@0.00": {
-                        slidesPerView: 1,
-                        spaceBetween: 10,
-                        navigation: false,
-                        pagination: false
-                    },
-                    "@1.00": {
-                        slidesPerView: 6,
-                        spaceBetween: 15,
-                    },
-                }}
-            >
-                {tabs.map((tab, index) => (
-                    <SwiperSlide key={index}>
-                        <button
-                            className={`tab-btn px-4 py-2 rounded flex flex-col items-center space-x-2 space-y-2 ${activeTab === index ? 'active' : 'in-active'}`}
-                            onClick={() => setActiveTab(index)}
+                            className=""
+                            spaceBetween={15}
+                            slidesPerView={6}
+                            loop={true}
+                            autoplay={{
+                                delay: 2500,
+                                disableOnInteraction: false,
+                            }}
+                            onBeforeInit={(swiper) => {
+                                swiperRef.current = swiper;
+                            }}
+                            modules={[Navigation, Autoplay, Pagination]}
+                            breakpoints={{
+                                "@0.00": {
+                                    slidesPerView: 1,
+                                    spaceBetween: 10,
+                                    navigation: false,
+                                    pagination: false
+                                },
+                                "@1.00": {
+                                    slidesPerView: 6,
+                                    spaceBetween: 15,
+                                },
+                            }}
                         >
-                            <Image src={tab.icon} alt="icons" width={70} height={70} />
-                            <span>{tab.name}</span>
-                        </button>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+                            {tabs.map((tab, index) => (
+                                <SwiperSlide key={index}>
+                                    <button
+                                        className={`tab-btn px-4 py-2 rounded flex flex-col items-center space-x-2 space-y-2 ${activeTab === index ? 'active' : 'in-active'}`}
+                                        onClick={() => setActiveTab(index)}
+                                    >
+                                        <Image src={tab.icon} alt="icons" width={70} height={70} />
+                                        <span>{tab.name}</span>
+                                    </button>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
                     </div>
-                    
 
-                   
+
+
 
                     {activeTab === 0 && (
                         <div className="flex flex-col lg:flex-row md:flex-row mt-10">
@@ -601,26 +601,28 @@ export default function PublishingLpNew() {
                     </h2>
                     <p className="mt-3 text-gray-600">Don't put off telling the world about your tale for much<br></br> longer. We make it inexpensive and simple.</p>
                 </div>
-                <div className="flex items-center justify-center gap-4">
-                    <div className="icon">
-                        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 13.5997 2.37562 15.1116 3.04346 16.4525C3.22094 16.8088 3.28001 17.2161 3.17712 17.6006L2.58151 19.8267C2.32295 20.793 3.20701 21.677 4.17335 21.4185L6.39939 20.8229C6.78393 20.72 7.19121 20.7791 7.54753 20.9565C8.88837 21.6244 10.4003 22 12 22Z" stroke="#fff" stroke-width="1.7" />
-                        </svg>
+                <div className="flex gap-10">
+                    <div className="flex flex-col lg:flex-row items-center justify-center gap-4">
+                        <div className="icon">
+                            <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 13.5997 2.37562 15.1116 3.04346 16.4525C3.22094 16.8088 3.28001 17.2161 3.17712 17.6006L2.58151 19.8267C2.32295 20.793 3.20701 21.677 4.17335 21.4185L6.39939 20.8229C6.78393 20.72 7.19121 20.7791 7.54753 20.9565C8.88837 21.6244 10.4003 22 12 22Z" stroke="#fff" stroke-width="1.7" />
+                            </svg>
+                        </div>
+                        <div className="content" onClick={handleOpenChat}>
+                            <h3>Chat With Us</h3>
+                            <p>Let's Talk</p>
+                        </div>
                     </div>
-                    <div className="content">
-                        <h3>Chat With Us</h3>
-                        <p>Let's Talk</p>
-                    </div>
-                </div>
-                <div className="flex items-center justify-center gap-4">
-                    <div className="icon">
-                        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M3 5.5C3 14.0604 9.93959 21 18.5 21C18.8862 21 19.2691 20.9859 19.6483 20.9581C20.0834 20.9262 20.3009 20.9103 20.499 20.7963C20.663 20.7019 20.8185 20.5345 20.9007 20.364C21 20.1582 21 19.9181 21 19.438V16.6207C21 16.2169 21 16.015 20.9335 15.842C20.8749 15.6891 20.7795 15.553 20.6559 15.4456C20.516 15.324 20.3262 15.255 19.9468 15.117L16.74 13.9509C16.2985 13.7904 16.0777 13.7101 15.8683 13.7237C15.6836 13.7357 15.5059 13.7988 15.3549 13.9058C15.1837 14.0271 15.0629 14.2285 14.8212 14.6314L14 16C11.3501 14.7999 9.2019 12.6489 8 10L9.36863 9.17882C9.77145 8.93713 9.97286 8.81628 10.0942 8.64506C10.2012 8.49408 10.2643 8.31637 10.2763 8.1317C10.2899 7.92227 10.2096 7.70153 10.0491 7.26005L8.88299 4.05321C8.745 3.67376 8.67601 3.48403 8.55442 3.3441C8.44701 3.22049 8.31089 3.12515 8.15802 3.06645C7.98496 3 7.78308 3 7.37932 3H4.56201C4.08188 3 3.84181 3 3.63598 3.09925C3.4655 3.18146 3.29814 3.33701 3.2037 3.50103C3.08968 3.69907 3.07375 3.91662 3.04189 4.35173C3.01413 4.73086 3 5.11378 3 5.5Z" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                    </div>
-                    <div className="content">
-                        <h3>Call Now</h3>
-                        <p>(866)-841-7469</p>
+                    <div className="flex flex-col lg:flex-row items-center justify-center gap-4">
+                        <div className="icon">
+                            <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M3 5.5C3 14.0604 9.93959 21 18.5 21C18.8862 21 19.2691 20.9859 19.6483 20.9581C20.0834 20.9262 20.3009 20.9103 20.499 20.7963C20.663 20.7019 20.8185 20.5345 20.9007 20.364C21 20.1582 21 19.9181 21 19.438V16.6207C21 16.2169 21 16.015 20.9335 15.842C20.8749 15.6891 20.7795 15.553 20.6559 15.4456C20.516 15.324 20.3262 15.255 19.9468 15.117L16.74 13.9509C16.2985 13.7904 16.0777 13.7101 15.8683 13.7237C15.6836 13.7357 15.5059 13.7988 15.3549 13.9058C15.1837 14.0271 15.0629 14.2285 14.8212 14.6314L14 16C11.3501 14.7999 9.2019 12.6489 8 10L9.36863 9.17882C9.77145 8.93713 9.97286 8.81628 10.0942 8.64506C10.2012 8.49408 10.2643 8.31637 10.2763 8.1317C10.2899 7.92227 10.2096 7.70153 10.0491 7.26005L8.88299 4.05321C8.745 3.67376 8.67601 3.48403 8.55442 3.3441C8.44701 3.22049 8.31089 3.12515 8.15802 3.06645C7.98496 3 7.78308 3 7.37932 3H4.56201C4.08188 3 3.84181 3 3.63598 3.09925C3.4655 3.18146 3.29814 3.33701 3.2037 3.50103C3.08968 3.69907 3.07375 3.91662 3.04189 4.35173C3.01413 4.73086 3 5.11378 3 5.5Z" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </div>
+                        <div className="content">
+                            <h3>Call Now</h3>
+                            <p><Link className="" href={'tel:8668417469'}>(866)-841-7469</Link></p>
+                        </div>
                     </div>
                 </div>
                 <div className="get-quote-btn">
@@ -824,28 +826,31 @@ export default function PublishingLpNew() {
                     </h2>
                     <p className="mt-3 text-gray-600">Don't put off telling the world about your tale for<br></br> much longer. We make it inexpensive and simple.</p>
                 </div>
-                <div className="flex items-center justify-center gap-4">
-                    <div className="icon">
-                        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 13.5997 2.37562 15.1116 3.04346 16.4525C3.22094 16.8088 3.28001 17.2161 3.17712 17.6006L2.58151 19.8267C2.32295 20.793 3.20701 21.677 4.17335 21.4185L6.39939 20.8229C6.78393 20.72 7.19121 20.7791 7.54753 20.9565C8.88837 21.6244 10.4003 22 12 22Z" stroke="#fff" stroke-width="1.7" />
-                        </svg>
+                <div className="flex gap-10">
+                    <div className="flex flex-col lg:flex-row items-center justify-center gap-4">
+                        <div className="icon">
+                            <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 13.5997 2.37562 15.1116 3.04346 16.4525C3.22094 16.8088 3.28001 17.2161 3.17712 17.6006L2.58151 19.8267C2.32295 20.793 3.20701 21.677 4.17335 21.4185L6.39939 20.8229C6.78393 20.72 7.19121 20.7791 7.54753 20.9565C8.88837 21.6244 10.4003 22 12 22Z" stroke="#fff" stroke-width="1.7" />
+                            </svg>
+                        </div>
+                        <div className="content" onClick={handleOpenChat}>
+                            <h3>Chat With Us</h3>
+                            <p>Let's Talk</p>
+                        </div>
                     </div>
-                    <div className="content">
-                        <h3>Chat With Us</h3>
-                        <p>Let's Talk</p>
+                    <div className="flex flex-col lg:flex-row items-center justify-center gap-4">
+                        <div className="icon">
+                            <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M3 5.5C3 14.0604 9.93959 21 18.5 21C18.8862 21 19.2691 20.9859 19.6483 20.9581C20.0834 20.9262 20.3009 20.9103 20.499 20.7963C20.663 20.7019 20.8185 20.5345 20.9007 20.364C21 20.1582 21 19.9181 21 19.438V16.6207C21 16.2169 21 16.015 20.9335 15.842C20.8749 15.6891 20.7795 15.553 20.6559 15.4456C20.516 15.324 20.3262 15.255 19.9468 15.117L16.74 13.9509C16.2985 13.7904 16.0777 13.7101 15.8683 13.7237C15.6836 13.7357 15.5059 13.7988 15.3549 13.9058C15.1837 14.0271 15.0629 14.2285 14.8212 14.6314L14 16C11.3501 14.7999 9.2019 12.6489 8 10L9.36863 9.17882C9.77145 8.93713 9.97286 8.81628 10.0942 8.64506C10.2012 8.49408 10.2643 8.31637 10.2763 8.1317C10.2899 7.92227 10.2096 7.70153 10.0491 7.26005L8.88299 4.05321C8.745 3.67376 8.67601 3.48403 8.55442 3.3441C8.44701 3.22049 8.31089 3.12515 8.15802 3.06645C7.98496 3 7.78308 3 7.37932 3H4.56201C4.08188 3 3.84181 3 3.63598 3.09925C3.4655 3.18146 3.29814 3.33701 3.2037 3.50103C3.08968 3.69907 3.07375 3.91662 3.04189 4.35173C3.01413 4.73086 3 5.11378 3 5.5Z" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </div>
+                        <div className="content">
+                            <h3>Call Now</h3>
+                            <p><Link className="" href={'tel:8668417469'}>(866)-841-7469</Link></p>
+                        </div>
                     </div>
                 </div>
-                <div className="flex items-center justify-center gap-4">
-                    <div className="icon">
-                        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M3 5.5C3 14.0604 9.93959 21 18.5 21C18.8862 21 19.2691 20.9859 19.6483 20.9581C20.0834 20.9262 20.3009 20.9103 20.499 20.7963C20.663 20.7019 20.8185 20.5345 20.9007 20.364C21 20.1582 21 19.9181 21 19.438V16.6207C21 16.2169 21 16.015 20.9335 15.842C20.8749 15.6891 20.7795 15.553 20.6559 15.4456C20.516 15.324 20.3262 15.255 19.9468 15.117L16.74 13.9509C16.2985 13.7904 16.0777 13.7101 15.8683 13.7237C15.6836 13.7357 15.5059 13.7988 15.3549 13.9058C15.1837 14.0271 15.0629 14.2285 14.8212 14.6314L14 16C11.3501 14.7999 9.2019 12.6489 8 10L9.36863 9.17882C9.77145 8.93713 9.97286 8.81628 10.0942 8.64506C10.2012 8.49408 10.2643 8.31637 10.2763 8.1317C10.2899 7.92227 10.2096 7.70153 10.0491 7.26005L8.88299 4.05321C8.745 3.67376 8.67601 3.48403 8.55442 3.3441C8.44701 3.22049 8.31089 3.12515 8.15802 3.06645C7.98496 3 7.78308 3 7.37932 3H4.56201C4.08188 3 3.84181 3 3.63598 3.09925C3.4655 3.18146 3.29814 3.33701 3.2037 3.50103C3.08968 3.69907 3.07375 3.91662 3.04189 4.35173C3.01413 4.73086 3 5.11378 3 5.5Z" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                    </div>
-                    <div className="content">
-                        <h3>Call Now</h3>
-                        <p>(866)-841-7469</p>
-                    </div>
-                </div>
+
                 <div className="get-quote-btn">
                     <button onClick={() => setIsOpen(true)}>REQUEST QUOTE</button>
                 </div>
