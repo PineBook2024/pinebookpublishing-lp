28152f21b074d9ecb65da4ea76b5f59e8db31b43
PineBook2024 <pinebookpublishing@gmail.com>
2024-09-23 15:17:17 -0700

    del lib folder

--- pages/lib/utils/index.js
+++
@@ -1,9 +0,0 @@
-export function formatDate(dateString, options = {}) {
-    const date = new Date(dateString);
-  
-    if (isNaN(date.getTime())) {
-      return 'Invalid Date';
-    }
-    return new Intl.DateTimeFormat('en-US', options).format(date);
-    
-  }
