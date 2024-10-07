edec91aa855dcc59a06746a2865e980576338c99
PineBook2024 <pinebookpublishing@gmail.com>
2024-10-07 12:30:13 -0700

    add chat zendesk

--- pages/_app.js
+++ pages/_app.js
@@ -181,21 +181,8 @@ export default function App({ Component, pageProps }) {
         />
         {/* <Script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=6ad75b0f-d085-4cae-9a7a-48abeb69b973"> </Script> */}
         {/* <Script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=6ad75b0f-d085-4cae-9a7a-48abeb69b973"> </Script> */}
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
-  ;(function(o,l,a,r,k,y){if(o.olark)return;
-  r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0];
-  y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r);
-  y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)};
-  y.extend=function(i,j){y("extend",i,j)};
-  y.identify=function(i){y("identify",k.i=i)};
-  y.configure=function(i,j){y("configure",i,j);k.c[i]=j};
-  k=y._={s:[],t:[+new Date],c:{},l:a};
-  })(window,document,"static.olark.com/jsclient/loader.js");/* custom configuration goes here (www.olark.com/documentation) */olark.identify('9674-763-10-6180');
-          `,
-          }}
-        />
+        <Script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=6ad75b0f-d085-4cae-9a7a-48abeb69b973"> </Script>
+
         {/* LiveAgent integration script */}
         {/* <script
           dangerouslySetInnerHTML={{
