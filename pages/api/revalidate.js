8d3494c2274be3d74eedd70d27abb48cefdacf75
PIne Book Publishing <167825260+PineBook2024@users.noreply.github.com>
2024-11-11 14:35:44 -0800

    new changes code blogs

---
+++ pages/api/revalidate.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+    if (req.query.secret !== process.env.API_ROUTE_SECRET) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+  
+    try {
+      await res.revalidate('/posts');
+      return res.json({ revalidated: true });
+    } catch (err) {
+      return res.status(500).send('Error revalidating');
+    }
+  }
+  
