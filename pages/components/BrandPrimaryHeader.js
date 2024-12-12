4ae2e818444b195f2214e1f9eabb722d5d39d19c
PIne Book Publishing <167825260+PineBook2024@users.noreply.github.com>
2024-12-12 14:42:06 -0800

    inner banner responsive fix

--- pages/components/BrandPrimaryHeader.js
+++ pages/components/BrandPrimaryHeader.js
@@ -11,7 +11,7 @@ export default function BrandPrimaryHeader(props) {
                     <div class="row py-5">
                         <div class="col-12 px-5 w-100 md:w-3/5">
                             <h4 className="text-white  font-poppins text-2xl">{props.subtitle}</h4>
-                            <h3 class="text-4xl text-white  font-poppins pt-2">
+                            <h3 class="text-3xl text-white  font-poppins pt-10">
                                 {props.title}</h3>
                             <p class="text-xl text-white pt-2">{props.desc}</p>
                         </div>
