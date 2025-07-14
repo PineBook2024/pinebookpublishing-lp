41d2c464c1670bb75d3428799184f0a3028c4bd7
PIne Book Publishing <167825260+PineBook2024@users.noreply.github.com>
2025-07-14 14:53:01 -0700

    404 page change

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import NewBrandFooter from "./components/NewBrandFooter";
-import BrandTopBar from "./components/BrandTopBar";
-import BrandNavbar from "./components/BrandNavbar";
 import Link from 'next/link';
 import Image from 'next/image';
 import Head from "next/head";
+import BrandNavbar from './components/BrandNavbar';
+import BrandFooter from './components/BrandFooter';
 
 
 
@@ -14,20 +13,13 @@ const notFound = () => {
   return (
     <div>
          <Head>
-                <title>Hire Professional Book Writers | 404</title>
+                <title>Hire Professional Book Publishing | 404</title>
                 <meta name="description" content="404"
                 />
                 <link rel="shortcut icon" href="/images/fav.webp" />
             </Head>
-      <header id="navbar" className="header-404 bg-transparent left-0 flex justify-between items-center p-4 z-10">
-                <div className='max-w-screen-xl flex justify-between items-center mx-auto w-full'>
-                    <div>
-                        <Link href="/" className="text-xl font-bold text-white">
-                            <Image className='logo' src={'/brand-img/logo.png'} width={200} height={80} alt="brand logo" loading="lazy" />
-                        </Link>
-                    </div>
-                </div>
-            </header>
+    
+    <BrandNavbar />
 
             <section className="sec404 overflow-hidden">
               <div className='container mx-auto'>
@@ -57,30 +49,7 @@ const notFound = () => {
               </div>
           </section>
 
-        <div className="copyright-sec copyright-sec-404">
-                    <div className="container text-center mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-                        <p className="text-white w-full md:text-center text-sm sm:text-left">
-                            Copyright © 2024 <span className="powered-by-text"><Link href="https://www.pinebookwriting.com/" >Pine Book Writing Inc.</Link></span> | All Rights Reserved.
-                            <Link
-                                href="/terms-and-conditions"
-                                rel="noopener noreferrer"
-                                className="text-white ml-1"
-                                target="_blank"
-                            >
-                                Terms & Conditions |
-                            </Link>
-                            <Link
-                                href="/privacy-policy"
-                                rel="noopener noreferrer"
-                                className="text-white ml-1"
-                                target="_blank"
-                            >
-                                Privacy Policy 
-                            </Link>
-                            {/* Powered by <span className="powered-by-text"><Link href="https://www.pinebookwriting.com/" target="_blank">Pine Book Writing Inc.</Link></span> */}
-                        </p>
-                    </div>
-                </div>
+       <BrandFooter />
       
     </div>
   )
