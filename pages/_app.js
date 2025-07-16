33a28162189f506ffa264fcdfa94bf022711c357
PIne Book Publishing <167825260+PineBook2024@users.noreply.github.com>
2025-07-16 14:53:13 -0700

    add scripts

--- pages/_app.js
+++ pages/_app.js
@@ -219,6 +219,16 @@ export default function App({ Component, pageProps }) {
             src="https://www.facebook.com/tr?id=1828587994272272&ev=PageView&noscript=1"
           />
         </noscript>
+    <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "lck76mjve8");`,
+          }}
+        />
       </main>
     </>
   );
