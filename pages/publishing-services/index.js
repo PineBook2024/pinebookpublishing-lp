38cbc02b52ea178406d239af048b951bf027338b
PIne Book Publishing <167825260+PineBook2024@users.noreply.github.com>
2025-12-04 13:48:53 -0800

    update code and lp new

--- pages/publishing-services/index.js
+++ pages/publishing-services/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
-import useHubspotForm from "@/hooks/hubspot";
+import useHubspotForm from "/hooks/hubspot";
 import Head from "next/head";
 import Image from "next/image";
 import Link from 'next/link';
