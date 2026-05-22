ed103e0f314e65580b29b189e14a1e5da387aead
unknown <zainabbasashtar55@gmail.com>
2026-05-23 03:23:08 +0500

    register url

--- pages/login.js
+++ pages/login.js
@@ -45,7 +45,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/login", {
+      const res = await fetch("https://pinebookbackend.pinedigitalhub.com/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
--- pages/register.js
+++ pages/register.js
@@ -54,7 +54,7 @@ export default function Register() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/api/register", {
+      const res = await fetch("https://pinebookbackend.pinedigitalhub.com/api/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
