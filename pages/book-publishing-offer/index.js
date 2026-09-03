15306f4ac3df0684c4a9d9e1dd2d3032ec6179df
PIne Book Publishing <167825260+PineBook2024@users.noreply.github.com>
2026-09-03 14:51:33 -0700

    add script tracking

--- pages/book-publishing-offer/index.js
+++ pages/book-publishing-offer/index.js
@@ -1119,6 +1119,39 @@ export default function Home() {
               `,
           }}
         />
+        <Script id="microsoft-uet" strategy="afterInteractive">
+          {`
+            (function(w, d, t, u, o) {
+              w[u] = w[u] || [];
+              o.ts = (new Date).getTime();
+
+              var n = d.createElement(t);
+              n.src = "https://bat.bing.net/bat.js?ti=" + o.ti +
+                ("uetq" != u ? "&q=" + u : "");
+
+              n.async = 1;
+
+              n.onload = n.onreadystatechange = function() {
+                var s = this.readyState;
+
+                if (!s || s === "loaded" || s === "complete") {
+                  o.q = w[u];
+                  w[u] = new UET(o);
+                  w[u].push("pageLoad");
+
+                  n.onload = n.onreadystatechange = null;
+                }
+              };
+
+              var i = d.getElementsByTagName(t)[0];
+              i.parentNode.insertBefore(n, i);
+
+            })(window, document, "script", "uetq", {
+              ti: "97267008",
+              enableAutoSpaTracking: true
+            });
+          `}
+        </Script>
       </Head>
       <main id="home" className="book-publishing-offer-page">
         <GoogleTranslateWidget />
